416b6bba4e1dfbbf02602f6c3248b28a5e5b8e5c
Abdullah <abdullahosama15@gmail.com>
2023-04-01 03:43:05 +0200

    multiple pings

--- app/server.go
+++ app/server.go
@@ -2,42 +2,61 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
+
+func failOnErr(err error, str string) {
+	if err != nil {
+		fmt.Println(str, err.Error())
+		os.Exit(1)
+	}
+}
+func handleConn(conn net.Conn) {
+
+	buff := make([]byte, 6)
+	for {
+		n, err := conn.Read(buff)
+
+		strArr := strings.Split(string(buff[:n]), "\r\n")
+
+		if len(strArr) > 0 && strArr[len(strArr)-1] == "\n" {
+			break
+		}
+		// fmt.Print(len(strArr), strArr)
+		if err != nil {
+			fmt.Println("Error reading sent data", err.Error())
+			os.Exit(1)
+		}
+
+		res := "+PONG\r\n"
+		writeBuffer := []byte(res)
+		_, err = conn.Write(writeBuffer)
+
+		if err != nil {
+			fmt.Println("Error writing data", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	// n, err = conn.Read(buff[:])
+	// fmt.Printf("%s", strings.Split(string(buff[:n]), "\r\n"))
+
+	conn.Close()
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	failOnErr(err, "Fail to listen")
+	for err == nil {
+		conn, err := l.Accept()
+		failOnErr(err, "Fail to Accept")
+		fmt.Println("Accepted a connection")
+		go handleConn(conn)
 
-	buff := make([]byte, 1024)
-	_, err = conn.Read(buff[:])
-
-	if err != nil {
-		fmt.Println("Error reading sent data", err.Error())
-		os.Exit(1)
 	}
-	res := "+PONG\r\n"
-	writeBuffer := []byte(res)
-	_, err = conn.Write(writeBuffer)
-
-	if err != nil {
-		fmt.Println("Error writing data", err.Error())
-		os.Exit(1)
-	}
-
 }
